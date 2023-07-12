4c1c08933a6eba544b4f05a1734ad313242ca9f0
yashikab <yashikaba@gmail.com>
2023-07-13 08:27:29 +0900

    update

--- tutorials/create_a_go_module/greetings/greetings.go
+++ tutorials/create_a_go_module/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -10,7 +11,17 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
